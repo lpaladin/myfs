7ee2bf0cdda03863c514413d2444b8a45b6e835d
Zakelly <Zakelly@163.com>
2015-02-20 15:15:26 +0800

    testing

--- hooks.c
+++ hooks.c
@@ -24,14 +24,37 @@ int myfs_hook_reg_delete(hook_func fun) {
 }
 EXPORT_SYMBOL(myfs_hook_reg_delete);
 
-
-
 /*
 * Modyfy below
 */
+
+static int inode_count;
+static int inode_size;
+
+static void printk_ana(){
+	printk("myfs inodes count is %d and size is %d .\n",inode_count,inode_size);
+}
+
+//自定义在创建inode时的钩子
+static int myfs_custom_inode_create_hook(struct inode * inode) {
+	inode_count++;
+	inode_size+=sizeof(struct inode);
+	printk_ana();
+}
+
+//自定义在删除inode时的钩子
+static int myfs_custom_inode_delete_hook(struct inode * inode) {
+	inode_count--;
+	inode_size-=sizeof(struct inode);
+	printk_ana();
+}
+
+//注册
 int myfs_hook_reg_entry(void) {
 	printk("hook_reg_entry\n");
-	myfs_hook_reg_create(void_inode);
-	myfs_hook_reg_delete(void_inode);
+	inode_count = 0;
+	inode_size = 0;
+	myfs_hook_reg_create(myfs_custom_inode_create_hook);
+	myfs_hook_reg_delete(myfs_custom_inode_delete_hook);
 	return -ENOSPC;
 }
